80e8409733939b5f59433736221be63f50131343
MaksimBocharov <maksimbocharov@gmail.com>
2020-03-10 16:10:16 +0200

    Gradle

--- TestBookLombok.java
+++ TestBookLombok.java
@@ -20,7 +20,6 @@ public class TestBookLombok {
         softly.assertThat(newBook.getYear()).isEqualTo(1906);
         softly.assertThat(newBook.getISBN()).isEqualTo("Eng-Something - 1234567890");
         softly.assertAll();
-
     }
 }
 
